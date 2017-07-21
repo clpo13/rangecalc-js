f89c4954346902aab02c95c098fc739597b53d30
Cody Logan <clpo13@gmail.com>
2017-07-21 11:41:42 -0700

    Add functions for converting IP to a decimal and getting the CIDR range of two addresses

--- bin/rangecalc.js
+++ bin/rangecalc.js
@@ -1,3 +1,34 @@
 #!/usr/bin/env node
 
 var ipaddr = require('ipaddr.js');
+
+var self = module.exports = {
+
+  getCIDR: function(start, end) {
+    var startInt = self.toInt(start);
+    var endInt = self.toInt(end);
+    console.log(startInt);
+    console.log(endInt);
+    var binaryXOR = startInt ^ endInt;
+    if (binaryXOR == 0) {
+      return 32;
+    } else {
+      var binaryStr = binaryXOR.toString(2);
+      var zeroCount = binaryStr.split("0").length - 1;
+      var oneCount = binaryStr.split("1").length - 1;
+      return 32 - (zeroCount + oneCount);
+    }
+  },
+
+  toInt: function(addr) {
+    var arr = addr.octets;
+    var ipInt = 0;
+    var counter = 3;
+    for (var i in arr) {
+      ipInt += arr[i] * Math.pow(256, counter);
+      counter--;
+    }
+    return ipInt;
+  }
+
+}
--- test/test.js
+++ test/test.js
@@ -1,1 +1,16 @@
 var assert = require('assert');
+var ipaddr = require('ipaddr.js');
+var rangecalc = require('../bin/rangecalc');
+
+describe('Rangecalc', function() {
+  it('should return the proper range', function() {
+    var start = ipaddr.parse("192.168.0.1");
+    var end = ipaddr.parse("192.168.0.254");
+    assert(rangecalc.getCIDR(start, end) == 24);
+  });
+
+  it('should return the right integer representation', function() {
+    var ip = ipaddr.parse("192.168.0.1");
+    assert(rangecalc.toInt(ip) == 3232235521);
+  });
+});
