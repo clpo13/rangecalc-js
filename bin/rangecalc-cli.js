8d8d08c0217aa6083ce5574fb2f65d4460c1a360
Cody Logan <clpo13@gmail.com>
2017-07-24 00:10:31 -0700

    Add eslint

--- bin/rangecalc-cli.js
+++ bin/rangecalc-cli.js
@@ -1,73 +1,72 @@
 #!/usr/bin/env node
 
-const ipaddr = require('ipaddr.js');
-const readline = require('readline');
-const rangecalc = require('../lib/rangecalc');
+const ipaddr = require('ipaddr.js')
+const readline = require('readline')
+const rangecalc = require('../lib/rangecalc')
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: 'rangecalc> ',
   completer: completer
-});
+})
 
-var ipAddresses = [];
+var ipAddresses = []
 
-console.log("Enter an IP address or type '.help'.");
-rl.prompt();
+console.log("Enter an IP address or type '.help'.")
+rl.prompt()
 
 rl.on('line', (line) => {
   switch (line.trim()) {
     case 'calc':
-      var arr = rangecalc.sort(ipAddresses);
-      var cidr = rangecalc.getCIDR(arr[0], arr[arr.length-1])
-      console.log('Range is ' + cidr);
-      break;
+      var arr = rangecalc.sort(ipAddresses)
+      var cidr = rangecalc.getCIDR(arr[0], arr[arr.length - 1])
+      console.log('Range is ' + cidr)
+      break
     case 'list':
       for (var i in ipAddresses) {
-        console.log(ipAddresses[i]);
+        console.log(ipAddresses[i])
       }
-      break;
+      break
     case 'clear':
-      ipAddresses = [];
-      console.log('IP list cleared');
-      break;
+      ipAddresses = []
+      console.log('IP list cleared')
+      break
     case '.help':
     case '.h':
       console.log('Input IP addresses one per line. Commands:\n' +
         '\tlist - shows current list of IP addresses\n' +
         '\tcalc - get the CIDR range\n' +
         '\t.help, .h - show this text\n' +
-        '\t.exit, .quit, .q - exit the program');
-      break;
+        '\t.exit, .quit, .q - exit the program')
+      break
     case '.exit':
     case '.quit':
     case '.q':
-      console.log('Bye!');
-      process.exit(0);
+      console.log('Bye!')
+      process.exit(0)
     default:
-      var input = line.trim();
+      var input = line.trim()
       if (ipaddr.isValid(input)) {
-        if (ipaddr.parse(input).kind() == 'ipv6') {
-          console.log("Sorry, IPv6 addresses are not currently supported!");
-          break;
+        if (ipaddr.parse(input).kind() === 'ipv6') {
+          console.log('Sorry, IPv6 addresses are not currently supported!')
+          break
         }
-        ipAddresses.push(input);
-        console.log(input + ' added to list');
+        ipAddresses.push(input)
+        console.log(input + ' added to list')
       } else {
-        console.log(input + ' is not a valid IP address');
+        console.log(input + ' is not a valid IP address')
       }
-      break;
+      break
   }
-  rl.prompt();
+  rl.prompt()
 }).on('close', () => {
-  console.log('Bye!');
-  process.exit(0);
-});
+  console.log('Bye!')
+  process.exit(0)
+})
 
-function completer(line) {
-  const completions = 'calc clear list .help .h .exit .quit .q'.split(' ');
-  const hits = completions.filter((c) => c.startsWith(line));
-  return [hits.length ? hits : completions, line];
+function completer (line) {
+  const completions = 'calc clear list .help .h .exit .quit .q'.split(' ')
+  const hits = completions.filter((c) => c.startsWith(line))
+  return [hits.length ? hits : completions, line]
 }
-
--- test/rangecalc-test.js
+++ test/rangecalc-test.js
@@ -1,38 +1,35 @@
-var assert = require('assert');
-var rangecalc = require('../lib/rangecalc');
+var assert = require('assert')
+var rangecalc = require('../lib/rangecalc')
 
-describe('Rangecalc', function() {
+describe('Rangecalc', function () {
+  describe('IPv4', function () {
+    it('should return the proper range', function () {
+      var start = '192.168.0.1'
+      var end = '192.168.0.254'
+      assert(rangecalc.getCIDR(start, end) === 24)
+    })
 
-  describe('IPv4', function() {
+    it('should return 32 when the IP addresses are identical', function () {
+      var start = '192.168.0.1'
+      var end = start
+      assert(rangecalc.getCIDR(start, end) === 32)
+    })
 
-    it('should return the proper range', function() {
-      var start = '192.168.0.1';
-      var end = '192.168.0.254';
-      assert(rangecalc.getCIDR(start, end) == 24);
-    });
+    it('should return the right integer representation', function () {
+      var ip = '192.168.0.1'
+      assert(rangecalc.toInt(ip) === 3232235521)
+    })
 
-    it('should return 32 when the IP addresses are identical', function() {
-      var start = '192.168.0.1';
-      var end = start;
-      assert(rangecalc.getCIDR(start, end) == 32);
-    });
-
-    it('should return the right integer representation', function() {
-      var ip = '192.168.0.1';
-      assert(rangecalc.toInt(ip) == 3232235521);
-    });
-
-    it('should order the addresses properly', function() {
+    it('should order the addresses properly', function () {
       var arr = ['192.168.0.1', '192.168.0.254', '192.168.0.50']
-      var sortedArr = rangecalc.sort(arr);
-      assert(sortedArr[2] == '192.168.0.254');
-    });
-  });
+      var sortedArr = rangecalc.sort(arr)
+      assert(sortedArr[2] === '192.168.0.254')
+    })
+  })
 
-  describe('IPv6', function() {
+  describe('IPv6', function () {
+    it('should return the proper range')
 
-    it('should return the proper range');
-
-    it('should return the right integer representation');
-  });
-});
+    it('should return the right integer representation')
+  })
+})
